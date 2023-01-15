028d3a18b813b9238d299bd399168410726d8f7d
fluorescia <104306794+fluorescia@users.noreply.github.com>
2023-01-15 10:41:40 +0800

    newspawn(v0.0.2): spirit ball enable

--- ball/spawnp.cpp
+++ ball/spawnp.cpp
@@ -9,32 +9,47 @@ void spawnp::OnLoad()
 	cmdspawnp_ptr = new cmdspawnp();
 	m_bml->RegisterCommand(cmdspawnp_ptr);
 	GetConfig()->SetCategoryComment("Main", "Main settings.");
+
 	prop_enabled = GetConfig()->GetProperty("Main", "Enabled");
 	prop_enabled->SetDefaultBoolean(true);
-	prop_enabled->SetComment("Enabled to use");
+	prop_enabled->SetComment("Enabled to use this mod");
 	enabled = prop_enabled->GetBoolean();
 
 	reset_prop_enabled = GetConfig()->GetProperty("Main", "Prop_Reset");
 	reset_prop_enabled->SetDefaultBoolean(false);
-	reset_prop_enabled->SetComment("Enable to reset prop when press shortcut key");
+	reset_prop_enabled->SetComment("Enable to reset prop when press Transport shortcut key.");
 	reset_enabled = reset_prop_enabled->GetBoolean();
 
-	prop_key = GetConfig()->GetProperty("Main", "Shortcut");
+	reset_spirit_enabled = GetConfig()->GetProperty("Main", "Spirit_Record");
+	reset_spirit_enabled->SetDefaultBoolean(true);
+	reset_spirit_enabled->SetComment("Enable to record and play spirit ball when press Record shortcut key.");
+	spirit_enabled = reset_spirit_enabled->GetBoolean();
+
+	prop_key = GetConfig()->GetProperty("Main", "Transport");
 	prop_key->SetDefaultKey(CKKEY_C);
-	prop_key->SetComment("Set your shortcut key");
-	key = prop_key->GetKey();
+	prop_key->SetComment("Set your shortcut key for transporting your ball");
+	key_prop = prop_key->GetKey();
+
+	prop_key_record = GetConfig()->GetProperty("Main", "Record");
+	prop_key_record->SetDefaultKey(CKKEY_V);
+	prop_key_record->SetComment("Set your shortcut key for saving the spirit record");
+	key_record = prop_key_record->GetKey();
 
 	input_manager = m_bml->GetInputManager();
-	
+
 }
 
 void spawnp::OnModifyConfig(CKSTRING category, CKSTRING key, IProperty* prop) {
-	//m_bml->SendIngameMessage("Config is modified.");
 	enabled = prop_enabled->GetBoolean();
 	reset_enabled = reset_prop_enabled->GetBoolean();
+	spirit_enabled = reset_spirit_enabled->GetBoolean();
 	if (prop == prop_key) {
-		this->key = prop->GetKey();
+		this->key_prop = prop->GetKey();
 	}
+	if (prop == prop_key_record) {
+		this->key_record = prop->GetKey();
+	}
+
 }
 
 void spawnp::OnLoadScript(CKSTRING filename, CKBehavior* script) {
@@ -45,23 +60,7 @@ void spawnp::OnLoadScript(CKSTRING filename, CKBehavior* script) {
 		m_phyNewBall = ScriptHelper::FindFirstBB(newBall, "physicalize new Ball");
 	}
 }
-/*
-void spawnp::OnStartLevel() {
-	//spawn sector set 1
-	cmdspawnp_ptr->set_curSector(1);
-	//spawn position set default
-	m_bml->AddTimer(10u, [this]() {
-		VxMatrix matrix = m_bml->Get3dEntityByName("Cam_OrientRef")->GetWorldMatrix();
-		for (int i = 0; i < 4; i++) {
-			std::swap(matrix[0][i], matrix[2][i]);
-			matrix[0][i] = -matrix[0][i];
-		}
-		cmdspawnp_ptr->set_matrix(matrix);
-		//get part 1 in spawndata
-		cmdspawnp_ptr->set_ball_type(ball_name_to_id[get_curBall()->GetName()]);
-	});
-}
-*/
+
 void spawnp::OnBallNavActive()
 {
 	Ball_Active = true;
@@ -85,32 +84,161 @@ void spawnp::OnPostStartMenu()
 		CKBehavior* id = ScriptHelper::FindNextBB(events, events->GetInput(0));
 		m_curSector = id->GetOutputParameter(0)->GetDestination(0);
 	}
-	
+
 	auto* script = m_bml->GetScriptByName("Gameplay_Ingame");
 	CKBehavior* trafoMgr = ScriptHelper::FindFirstBB(script, "Trafo Manager");
 	m_setNewBall = ScriptHelper::FindFirstBB(trafoMgr, "set new Ball");
 	CKBehavior* sop = ScriptHelper::FindFirstBB(m_setNewBall, "Switch On Parameter");
 	m_curTrafo = sop->GetInputParameter(0)->GetDirectSource();
-	
+
 	init = true;
 }
 
-void spawnp::OnProcess() {
-	if (!(enabled && m_bml->IsCheatEnabled() && m_bml->IsPlaying() && Ball_Active))
-		return;
-	if (input_manager->IsKeyPressed(key)) {
+void spawnp::OnLoadObject(CKSTRING filename, BOOL isMap, CKSTRING masterName, CK_CLASSID filterClass, BOOL addtoscene, BOOL reuseMeshes, BOOL reuseMaterials, BOOL dynamic, XObjectArray* objArray, CKObject* masterObj)
+{
+	if (!strcmp(filename, "3D Entities\\Balls.nmo")) {
+		CKDataArray* physBall = m_bml->GetArrayByName("Physicalize_GameBall");
+		for (int i = 0; i < physBall->GetRowCount(); i++) {
+			SpiritBall ball;
+			ball.name.resize(physBall->GetElementStringValue(i, 0, nullptr), '\0');
+			physBall->GetElementStringValue(i, 0, &ball.name[0]);
+			ball.name.pop_back();
+			ball.obj = m_bml->Get3dObjectByName(ball.name.c_str());
 
-		///sector judge
-		//get spawn'sector
-		// 
-		//m_bml->AddTimer(3u, [this]() {
+			CKDependencies dep;
+			dep.Resize(40); dep.Fill(0);
+			dep.m_Flags = CK_DEPENDENCIES_CUSTOM;
+			dep[CKCID_OBJECT] = CK_DEPENDENCIES_COPY_OBJECT_NAME | CK_DEPENDENCIES_COPY_OBJECT_UNIQUENAME;
+			dep[CKCID_MESH] = CK_DEPENDENCIES_COPY_MESH_MATERIAL;
+			dep[CKCID_3DENTITY] = CK_DEPENDENCIES_COPY_3DENTITY_MESH;
+			ball.obj = static_cast<CK3dObject*>(m_bml->GetCKContext()->CopyObject(ball.obj, &dep, "_Spirit"));
+			for (int j = 0; j < ball.obj->GetMeshCount(); j++) {
+				CKMesh* mesh = ball.obj->GetMesh(j);
+				for (int k = 0; k < mesh->GetMaterialCount(); k++) {
+					CKMaterial* mat = mesh->GetMaterial(k);
+					mat->EnableAlphaBlend();
+					mat->SetSourceBlend(VXBLEND_SRCALPHA);
+					mat->SetDestBlend(VXBLEND_INVSRCALPHA);
+					VxColor color = mat->GetDiffuse();
+					color.a = 0.5f;
+					mat->SetDiffuse(color);
+					ball.materials.push_back(mat);
+					m_bml->SetIC(mat);
+				}
+			}
+			m_dualBalls.push_back(ball);
+		}
+
+		GetLogger()->Info("Created Newspawn Spirit Balls");
+	}
+
+	if (!isMap || strlen(filename) < 1)
+		return;
+	std::string path;
+	if (filename[0] == '3')
+		path = "..\\";
+	path += filename;
+
+	std::ifstream map_file(path, std::ios::binary);
+	MD5 md5_stream;
+	auto buffer = std::make_unique_for_overwrite<char[]>(READ_SIZE);
+
+	while (!map_file.eof()) {
+		map_file.read(buffer.get(), READ_SIZE);
+		md5_stream.add(buffer.get(), map_file.gcount());
+	}
+	//m_bml->SendIngameMessage(md5_stream.getHash().c_str());
+	set_map_hash_curlevel(md5_stream.getHash());
+}
+
+int spawnp::GetCurrentBall(){
+	//get and return the balltype number
+	CKObject* ball = m_curLevel->GetElementObject(0, 1);
+	if (ball) {
+		std::string ballName = ball->GetName();
+		for (size_t i = 0; i < m_dualBalls.size(); i++) {
+			if (m_dualBalls[i].name == ballName)
+				return i;
+		}
+	}
+	return 0;
+}
+
+void spawnp::SetCurrentBall(int curBall) {
+	m_playBall = curBall;
+	for (auto& ball : m_dualBalls)
+		ball.obj->Show(CKHIDE);
+}
+
+void spawnp::StopRecording(bool save_data) {
+	m_isRecording = false;
+	if (save_data) {
+		m_play = m_record;
+		record_exist = true;
+	}
+	else {
+		m_record.trafo.clear();
+		m_record.trafo.shrink_to_fit();
+		m_record.states.clear();
+		m_record.states.shrink_to_fit();
+	}
+}
+
+void spawnp::StopPlaying() {
+	m_isPlaying = false;
+
+	for (auto& ball : m_dualBalls)
+		ball.obj->Show(CKHIDE);
+}
+
+void spawnp::StartRecording() {
+	m_isRecording = true;
+	m_recordTimer = 0;
+	m_srtimer = 0;
+	m_curBall = GetCurrentBall();
+	m_record.trafo.push_back({ 0, m_curBall });
+}
+
+void spawnp::StartPlaying() {
+	if (record_exist) {
+		m_isPlaying = true;
+		m_playTimer = -1000.0f / TICK_SPEED;
+		m_playFrame = 0;
+		m_playTrafo = 1;
+		SetCurrentBall(m_play.trafo[0].second);
+	}
+}
+
+void spawnp::OnProcess() {
+	if (!(enabled && m_bml->IsCheatEnabled() && m_bml->IsPlaying())) 
+		return;
+	if (input_manager->IsKeyPressed(key_prop) && Ball_Active) {
+		//[transport] no nsp or no set level: return
+		if (!(cmdspawnp_ptr->get_is_spawned())) {
+			m_bml->SendIngameMessage("You have not set up a nsp point.");
+			return;
+		}
+		if (cmdspawnp_ptr->get_map_hash_setlevel() != get_map_hash_curlevel()) {
+			m_bml->SendIngameMessage("the nsp point you set is not in this level.");
+			return;
+		}
+
+		//[spirit] Make sure you reset the ball before saving the replay
+		record_save_enable = true;
+
+		//[spirit] Press shortcut key, End last playing and recording
+		StopRecording();
+		StopPlaying();
+
+
+		//[transport] Get the sector at the time of /nsp
 		int set_sector = cmdspawnp_ptr->get_curSector();
-		///get current sector
+		//[transport] Get the current sector
 		CKBehavior* events = m_bml->GetScriptByName("Gameplay_Events");
 		int cur_sector = ScriptHelper::GetParamValue<int>(ScriptHelper::FindNextBB(events, events->GetInput(0))->GetOutputParameter(0)->GetDestination(0));
-		//do sector if yes
+		
+		//[transport] Switch sector if (set_sector != cur_sector)
 		CKContext* ctx = m_bml->GetCKContext();
-
 		if (m_curLevel) {
 			if (set_sector != cur_sector) {
 
@@ -158,17 +286,17 @@ void spawnp::OnProcess() {
 								m_curLevel->SetElementValue(0, 4, &active);
 								ctx->GetCurrentScene()->Activate(flame->GetScript(0), true);
 								m_bml->Show(flame, CKSHOW, true);
-							});
+								});
 						}
-					});
+						});
 
 					return false;
-				});
+					});
 
 			}
 		}
-		//	});
-		//m_bml->AddTimer(3u, [this]() {
+
+		//[transport] Reset camera
 		CKDataArray* ph = m_bml->GetArrayByName("PH");
 		for (int i = 0; i < ph->GetRowCount(); i++) {
 			CKBOOL set = true;
@@ -177,14 +305,16 @@ void spawnp::OnProcess() {
 			if (!strcmp(name, "P_Extra_Point"))
 				ph->SetElementValue(i, 4, &set);
 		}
+
+		//[transport] Reset props
 		if (reset_enabled) {
 			m_ingameParam->SetElementValueFromParameter(0, 1, m_curSector);
 			m_ingameParam->SetElementValueFromParameter(0, 2, m_curSector);
 			CKBehavior* sectorMgr = m_bml->GetScriptByName("Gameplay_SectorManager");
 			ctx->GetCurrentScene()->Activate(sectorMgr, true);
 		}
-		//}); 
-	///ball position tp
+
+		//[transport] Ball position transport
 		CKMessageManager* mm = m_bml->GetMessageManager();
 		CKMessageType ballDeact = mm->AddMessageType("BallNav deactivate");
 		mm->SendMessageSingle(ballDeact, m_bml->GetGroupByName("All_Gameplay"));
@@ -193,7 +323,7 @@ void spawnp::OnProcess() {
 		m_dynamicPos->Activate();
 		auto current_ball = get_curBall();
 
-		m_bml->AddTimer(2u, [this, current_ball,mm,ballDeact]() {
+		m_bml->AddTimer(2u, [this, current_ball, mm, ballDeact]() {
 			ExecuteBB::Unphysicalize(current_ball);
 
 			auto matrix = cmdspawnp_ptr->get_matrix();
@@ -211,10 +341,7 @@ void spawnp::OnProcess() {
 			mm->SendMessageSingle(ballDeact, m_bml->GetGroupByName("All_Gameplay"));
 			mm->SendMessageSingle(ballDeact, m_bml->GetGroupByName("All_Sound"));
 
-
-			//});
-			//m_bml->AddTimer(5u, [this]() {
-
+			//[transport] Ball type reset
 			CK3dEntity* curBall = get_curBall();
 			ExecuteBB::Unphysicalize(curBall);
 			static char trafoTypes[3][6] = { "paper", "wood", "stone" };
@@ -224,27 +351,105 @@ void spawnp::OnProcess() {
 			GetLogger()->Info("NewSpawn Reset Ball");
 		});
 
-		//if (ball_name_to_id[current_ball->GetName()] != cmdspawnp_ptr->get_ball_type()) {
-		//
-		//	m_bml->AddTimer(3u, [this, mm, ballDeact]() {
-		//		mm->SendMessageSingle(ballDeact, m_bml->GetGroupByName("All_Gameplay"));
-		//		mm->SendMessageSingle(ballDeact, m_bml->GetGroupByName("All_Sound"));
-		//	
-		//	
-		//	//});
-		//	//m_bml->AddTimer(5u, [this]() {
-		//
-		//		CK3dEntity* curBall = get_curBall();
-		//		ExecuteBB::Unphysicalize(curBall);
-		//		static char trafoTypes[3][6] = { "paper", "wood", "stone" };
-		//		SetParamString(m_curTrafo, trafoTypes[cmdspawnp_ptr->get_ball_type()]);
-		//		m_setNewBall->ActivateInput(0);
-		//		m_setNewBall->Activate();
-		//		GetLogger()->Info("NewSpawn Reset Ball");
-		//	});
-		//}
+		//[spirit] Transport end, start recording and playing if enable
+		if (spirit_enabled) {
+			StartRecording();
+			StartPlaying();
+		}
+
+	}
+
+
+	if (!spirit_enabled)
+		return;
+	
+	const float delta = 1000.0f / TICK_SPEED;
+	//[spirit] Recording
+	if (m_isRecording) {
+		m_srtimer += m_bml->GetTimeManager()->GetLastDeltaTime();
+
+		m_recordTimer += m_bml->GetTimeManager()->GetLastDeltaTime();
+		m_recordTimer = (std::min)(m_recordTimer, 1000.0f);
+
+		while (m_recordTimer > 0) {
+			m_recordTimer -= delta;
+
+			int curBall = GetCurrentBall();
+			if (curBall != m_curBall)
+				m_record.trafo.push_back({ m_record.states.size(), curBall });
+
+			m_curBall = curBall;
+
+			CK3dObject* ball = static_cast<CK3dObject*>(m_curLevel->GetElementObject(0, 1));
+			if (ball) {
+				Record::State state;
+				ball->GetPosition(&state.pos);
+				ball->GetQuaternion(&state.rot);
+				m_record.states.push_back(state);
+			}
+		}
+		if (m_srtimer > 1000 * 1800) {
+			GetLogger()->Info("NewSpawn Record is longer than half hour, stop recording");
+			StopPlaying();
+			StopRecording(true);
+		}
+	}
+
+	//[spirit] Playing
+	if (m_isPlaying) {
+		m_playTimer += m_bml->GetTimeManager()->GetLastDeltaTime();
+		m_playTimer = (std::min)(m_playTimer, 1000.0f);
+
+		while (m_playTimer > 0) {
+			m_playTimer -= delta;
+			m_playFrame++;
+
+			auto& trafos = m_play.trafo;
+			if (m_playTrafo < trafos.size()) {
+				auto& trafo = trafos[m_playTrafo];
+				if (m_playFrame == trafo.first) {
+					SetCurrentBall(trafo.second);
+					m_playTrafo++;
+				}
+			}
+		}
+
+		auto& states = m_play.states;
+		if (m_playFrame < states.size() - 1) {
+			if (m_playFrame >= 1) {
+				if (m_playBall >= 0 && m_playBall < m_dualBalls.size()) {
+					CKObject* playerBall = m_curLevel->GetElementObject(0, 1);
+					CK3dObject* ball = m_dualBalls[m_playBall].obj;
+					ball->Show(playerBall->IsVisible() ? CKSHOW : CKHIDE);
+
+					float portion = (m_playTimer / delta + 1);
+					Record::State& cur = states[m_playFrame], & next = states[m_playFrame + 1];
+					VxVector position = (next.pos - cur.pos) * portion + cur.pos;
+					VxQuaternion rotation = Slerp(portion, cur.rot, next.rot);
+					ball->SetPosition(position);
+					ball->SetQuaternion(rotation);
+				}
+			}
+		}
+		else StopPlaying();
+
+	}
+
+	//[spirit] Recording saving
+	if (input_manager->IsKeyPressed(key_record)) {
+		if (record_save_enable) {
+			StopPlaying();
+			StopRecording(true);
+			m_bml->SendIngameMessage("record is updated.");
+		}
+		else {
+			m_bml->SendIngameMessage("Please use transport shortcut key to reset the ball position first.");
+		}
 	}
 }
+
+
+
 
 //////////////////cmdspawnp
 
@@ -271,6 +476,11 @@ void cmdspawnp::Execute(IBML* bml, const std::vector<std::string>& args)
 			+ std::to_string(matrix[3][0]) + ", "
 			+ std::to_string(matrix[3][1]) + ", "
 			+ std::to_string(matrix[3][2]) + ")").c_str());
+		//get spawn level and is spawned
+		///
+		set_map_hash_setlevel(spawnp::this_instance->get_map_hash_curlevel());
+		set_is_spawned(true);
+
 	}
 }
 
--- ball/spawnp.h
+++ ball/spawnp.h
@@ -1,11 +1,18 @@
 #pragma once
 #include <BML/BMLAll.h>
+#include <fstream>
+#include <md5.h>
+
+#define TICK_SPEED 8
 
 extern "C" {
 	__declspec(dllexport) IMod* BMLEntry(IBML* bml);
 }
 
-std::unordered_map<std::string, int> ball_name_to_id{{"Ball_Paper", 0}, {"Ball_Wood", 1}, {"Ball_Stone", 2}};
+constexpr int READ_SIZE = 1024 * 16;
+
+
+std::unordered_map<std::string, int> ball_name_to_id{ {"Ball_Paper", 0}, {"Ball_Wood", 1}, {"Ball_Stone", 2} };
 
 class cmdspawnp : public ICommand
 {
@@ -17,15 +24,26 @@ public:
 	void Execute(IBML* bml, const std::vector<std::string>& args) override;
 	const std::vector<std::string> GetTabCompletion(IBML* bml, const std::vector<std::string>& args) override;
 	const VxMatrix& get_matrix() const { return matrix; }
+	void set_matrix(const VxMatrix& mat) { matrix = mat; }
 	int get_curSector() const { return curSector; }
+	void set_curSector(const int sec) { curSector = sec; }
+
 	int get_ball_type() const { return ball_type; }
 	void set_ball_type(const int type) { ball_type = type; }
-	void set_matrix(const VxMatrix& mat) { matrix = mat; }
-	void set_curSector(const int sec) { curSector = sec; }
+
+	bool get_is_spawned() const { return is_spawned; }
+	void set_is_spawned(const bool type) { is_spawned = type; }
+
+	std::string get_map_hash_setlevel() const { return map_hash_setlevel; }
+	void set_map_hash_setlevel(const std::string type) { map_hash_setlevel = type; }
 private:
 	VxMatrix matrix{};
 	int curSector{};
 	int ball_type{};
+
+	bool is_spawned = false;
+
+	std::string map_hash_setlevel;
 };
 
 class spawnp : public IMod {
@@ -33,46 +51,110 @@ public:
 	spawnp(IBML* bml) : IMod(bml) { this_instance = this; }
 
 	virtual CKSTRING GetID() override { return "NewSpawn"; }
-	virtual CKSTRING GetVersion() override { return "0.0.1"; }
+	virtual CKSTRING GetVersion() override { return "0.0.2"; }
 	virtual CKSTRING GetName() override { return "NewSpawn"; }
 	virtual CKSTRING GetAuthor() override { return "fluoresce"; }
-	virtual CKSTRING GetDescription() override { return "\"newspawn\" or \"nsp\" to set a position. shortcut key to tp to the position but do not reset the prop. reset the prop and tp to sector if the current is not the set sector.Thanks for the help of Bug and Swung "; }
+	virtual CKSTRING GetDescription() override { return "\"nsp\" to set a position. shortcut key to tp to the position. Thanks for the help of BallanceBug and Swung "; }
 	DECLARE_BML_VERSION;
 	//VxMatrix matrix;
 	CK3dEntity* get_curBall() { return static_cast<CK3dEntity*>(m_curLevel->GetElementObject(0, 1)); }
 	inline static spawnp* this_instance = nullptr;
-
-private: 
+	std::string get_map_hash_curlevel() { return map_hash_curlevel; }
+	void set_map_hash_curlevel(const std::string type) { map_hash_curlevel = type; }
+private:
+	//[transport]
 	void OnLoad() override;
 	void OnProcess() override;
 	void OnModifyConfig(CKSTRING category, CKSTRING key, IProperty* prop) override;
 	void OnLoadScript(CKSTRING filename, CKBehavior* script) override;
-	//void OnStartLevel() override;
+
 	void OnBallNavActive() override;
 	void OnBallNavInactive() override;
 	void OnPostStartMenu() override;
+	void OnLoadObject(CKSTRING filename, BOOL isMap, CKSTRING masterName,
+		CK_CLASSID filterClass, BOOL addtoscene, BOOL reuseMeshes, BOOL reuseMaterials,
+		BOOL dynamic, XObjectArray* objArray, CKObject* masterObj) override;
 
+	//[spirit]
+	void OnPreResetLevel() override { StopRecording(); StopPlaying(); record_save_enable = false;};
+	void OnPreExitLevel() override { StopRecording(); StopPlaying(); record_save_enable = false;};
+	void OnPreEndLevel() override { StopRecording(); StopPlaying(); record_save_enable = false;};
 
+	int GetCurrentBall();
+	void SetCurrentBall(int curBall);
+
+	void StopPlaying();
+	void StopRecording(bool save_data = false);
+
+	void StartRecording();
+	void StartPlaying();
+
+	//[transport]
 	bool init = false;
 	bool Ball_Active = false;
-	IProperty *prop_enabled, *reset_prop_enabled, *prop_key;
+	IProperty* prop_enabled, * reset_prop_enabled, * prop_key, * reset_spirit_enabled, * prop_key_record;
 	InputHook* input_manager;
-	CKKEYBOARD key;
+	CKKEYBOARD key_prop, key_record;
 	bool enabled = false;
 	bool reset_enabled = false;
+	bool spirit_enabled = false;
 	cmdspawnp* cmdspawnp_ptr = nullptr;
 
 	CKBehavior* m_dynamicPos = nullptr;
 	CKBehavior* m_phyNewBall = nullptr;
 	CKDataArray* m_curLevel, * m_checkpoints, * m_resetpoints, * m_ingameParam;
 	CKParameter* m_curSector;
-	//
+
+	//[transport] Ball type
 	CKParameter* m_curTrafo;
 	CKBehavior* m_setNewBall;
 	void SetParamString(CKParameter* param, CKSTRING value) {
 		param->SetStringValue(value);
 	}
+
+	//[transport] Map hash
+	std::string map_hash_curlevel;
+
+	//[spirit]
+	bool m_isRecording = false;
+	bool m_isPlaying = false;
+
+	bool record_exist = false;
+
+	bool record_save_enable = false;
+
+	float m_recordTimer;
+	float m_playTimer;
+	float m_srtimer;
+
+	struct SpiritBall {
+		std::string name;
+		CK3dObject* obj;
+		std::vector<CKMaterial*> materials;
+	};
+	std::vector<SpiritBall> m_dualBalls;
+
+	struct Record {
+		struct State {
+			VxVector pos;
+			VxQuaternion rot;
+		};
+
+		std::vector<State> states;
+		std::vector<std::pair<int, int>> trafo;
+	};
+	Record m_record, m_play;
+
+	size_t m_playBall;
+	size_t m_curBall;
+	size_t m_playFrame, m_playTrafo;
 };
+
+
+
+
+
+
 namespace sectorget {
 	template<typename T>
 	T GetParamValue(CKParameter* param) {
